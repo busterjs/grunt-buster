45980dfb577b2913b8e047924c04dd6d3bd2a1df
Szymon Przybylski <szymon.przybylski@allegro.pl>
2013-04-19 10:14:39 +0200

    reformat code

--- tasks/buster.js
+++ tasks/buster.js
@@ -1,229 +1,229 @@
-module.exports = function(grunt) {
-  var childProcess = require('child_process'),
-      path = require('path'),
-      when = require('when'),
-      phantomjs = require('phantomjs'),
-      growl;
+module.exports = function (grunt) {
+    var childProcess = require('child_process'),
+        path = require('path'),
+        when = require('when'),
+        phantomjs = require('phantomjs'),
+        growl;
 
-  try {
-    growl = require('growl');
-  } catch(e) {
-    growl = Function.prototype;
-    grunt.verbose.writeln('Growl not found, `npm install growl` for Growl support'.yellow);
-  }
-
-  var getConfigSection = function(cmd){
-    return (grunt.config('buster') || {})[cmd] || {};
-  };
-
-  var getArguments = function(cmd) {
-    if(cmd === 'phantomjs'){
-      var port = getConfigSection('server').port || 1111,
-          url = 'http://localhost:' + port + '/capture';
-      return [__dirname + '/buster/phantom.js', url];
+    try {
+        growl = require('growl');
+    } catch (e) {
+        growl = Function.prototype;
+        grunt.verbose.writeln('Growl not found, `npm install growl` for Growl support'.yellow);
     }
 
-    var args = [],
-        config = getConfigSection(cmd);
-
-    for(var arg in config){
-      var value = config[arg];
-      if(value !== false) {
-        args.push('--' + arg);
-        if(value !== true){
-          args.push(value);
-        }
-      }
-    }
-    return args;
-  };
-
-  var shouldRunServer = function(){
-    var configFile = getConfigSection('test').config;
-    if(!configFile){
-      grunt.verbose.writeln('No buster configuration specified. Looking for buster.js...');
-
-      if(path.existsSync('buster.js')) {
-        configFile = 'buster.js';
-        grunt.verbose.writeln('Found buster.js');
-      } else if(path.existsSync('test/buster.js')) {
-        configFile = 'test/buster.js';
-        grunt.verbose.writeln('Found test/buster.js');
-      } else if(path.existsSync('spec/buster.js')) {
-        configFile = 'spec/buster.js';
-        grunt.verbose.writeln('Found spec/buster.js');
-      }
-    }
-    var configs = require(path.join(process.cwd(), configFile));
-
-    for(var config in configs){
-      if((configs[config].environment || configs[config].env) === 'browser'){
-        return true;
-      }
-    }
-  };
-
-  var busterNotFound = function(){
-    grunt.log.errorlns(
-          'In order for this task to work properly, Buster.JS must be ' +
-          'installed and in the system PATH (if you can run "buster" at' +
-          'the command line, this task should work).' +
-          'To install Buster.JS, run `npm install -g buster`.'
-        );
-  };
-
-  var phantomjsNotFound = function(){
-    grunt.log.errorlns(
-          'In order for this task to work properly, PhantomJS must be ' +
-          'installed and in the system PATH (if you can run "phantomjs" at' +
-          'the command line, this task should work). Unfortunately, ' +
-          'PhantomJS cannot be installed automatically via npm or grunt. ' +
-          'See the grunt FAQ for PhantomJS installation instructions: ' +
-          'https://github.com/cowboy/grunt/blob/master/docs/faq.md'
-        );
-  };
-
-  var runBusterServer = function(){
-    var deferred = when.defer(),
-        busterServerPath = path.resolve(__dirname, '../node_modules/.bin/buster-server');
-
-    var server = childProcess.spawn(busterServerPath, getArguments('server'), {
-      env: process.env,
-      setsid: true
-    });
-
-    server.stdout.once('data', function(data) {
-      deferred.resolve(server);
-    });
-
-    server.stderr.once('data', function(data) {
-      deferred.reject(server);
-    });
-
-    server.stdout.on('data', function(data) {
-      process.stdout.write(data);
-    });
-
-    server.stderr.on('data', function(data) {
-      process.stderr.write(data);
-    });
-
-    return deferred.promise;
-  };
-
-  var runBusterTest = function(){
-    var deferred = when.defer(),
-        busterTestPath = path.resolve(__dirname, '../node_modules/.bin/buster-test'),
-        output = [];
-
-    var run = childProcess.spawn(busterTestPath, getArguments('test'), {
-      env: process.env,
-      setsid: true
-    });
-
-    run.stdout.on('data', function(data) {
-      output.push(data);
-      process.stdout.write(data);
-    });
-
-    run.stderr.on('data', function(data) {
-      process.stderr.write(data);
-    });
-
-    run.on('exit', function(code) {
-      var text = '';
-      if(output[output.length - 2]){
-        text = output[output.length - 2].toString().split(', ').join('\n') + output[output.length - 1];
-      }
-      text = text.replace(/\u001b\[.*m/g, '').trim();
-      if(code === 0){
-        growl(text, {
-          title: 'Tests Passed',
-          image: __dirname + '/buster/ok.png'
-        });
-        deferred.resolve();
-      }
-      else {
-        growl(text, {
-          title: 'Tests Failed',
-          image: __dirname + '/buster/error.png'
-        });
-        deferred.reject();
-      }
-    });
-
-    return deferred.promise;
-  };
-
-  var runPhantomjs = function() {
-    var deferred = when.defer();
-    var server = childProcess.spawn(phantomjs.path, getArguments('phantomjs'), {
-      env: process.env,
-      setsid: true
-    });
-
-    server.stdout.on('data', function(data) {
-      grunt.verbose.writeln(data);
-    });
-
-    server.stderr.on('data', function(data) {
-      grunt.verbose.writeln(data);
-    });
-
-    server.stdout.once('data', function() {
-      deferred.resolve(server);
-    });
-
-    return deferred.promise;
-  };
-
-  grunt.registerTask('buster', 'Run Buster.JS tests.', function() {
-    var done = this.async();
-    var stop = function(success, server, phantomjs){
-      if(server){
-        server.kill();
-        grunt.verbose.writeln('buster-server stopped');
-      }
-      if(phantomjs){
-        phantomjs.kill();
-        grunt.verbose.writeln('phantomjs stopped');
-      }
-      done(success);
+    var getConfigSection = function (cmd) {
+        return (grunt.config('buster') || {})[cmd] || {};
     };
 
-    if(shouldRunServer()){
-      runBusterServer().then(
-        function(server){
-          runPhantomjs().then(
-            function(phantomjs){
-              runBusterTest().then(
-                function(){
-                  stop(null, server, phantomjs);
-                },
-                function(){
-                  stop(false, server, phantomjs);
+    var getArguments = function (cmd) {
+        if (cmd === 'phantomjs') {
+            var port = getConfigSection('server').port || 1111,
+                url = 'http://localhost:' + port + '/capture';
+            return [__dirname + '/buster/phantom.js', url];
+        }
+
+        var args = [],
+            config = getConfigSection(cmd);
+
+        for (var arg in config) {
+            var value = config[arg];
+            if (value !== false) {
+                args.push('--' + arg);
+                if (value !== true) {
+                    args.push(value);
                 }
-              );
-            },
-            function(phantomjs) {
-              stop(false, server, phantomjs);
             }
-          );
-        },
-        function(server){
-          stop(false, server);
         }
-      );
-    }
-    else {
-      runBusterTest().then(
-        function(){
-          done(null);
-        },
-        function(){
-          done(false);
+        return args;
+    };
+
+    var shouldRunServer = function () {
+        var configFile = getConfigSection('test').config;
+        if (!configFile) {
+            grunt.verbose.writeln('No buster configuration specified. Looking for buster.js...');
+
+            if (path.existsSync('buster.js')) {
+                configFile = 'buster.js';
+                grunt.verbose.writeln('Found buster.js');
+            } else if (path.existsSync('test/buster.js')) {
+                configFile = 'test/buster.js';
+                grunt.verbose.writeln('Found test/buster.js');
+            } else if (path.existsSync('spec/buster.js')) {
+                configFile = 'spec/buster.js';
+                grunt.verbose.writeln('Found spec/buster.js');
+            }
         }
-      );
-    }
-  });
+        var configs = require(path.join(process.cwd(), configFile));
+
+        for (var config in configs) {
+            if ((configs[config].environment || configs[config].env) === 'browser') {
+                return true;
+            }
+        }
+    };
+
+    var busterNotFound = function () {
+        grunt.log.errorlns(
+            'In order for this task to work properly, Buster.JS must be ' +
+                'installed and in the system PATH (if you can run "buster" at' +
+                'the command line, this task should work).' +
+                'To install Buster.JS, run `npm install -g buster`.'
+        );
+    };
+
+    var phantomjsNotFound = function () {
+        grunt.log.errorlns(
+            'In order for this task to work properly, PhantomJS must be ' +
+                'installed and in the system PATH (if you can run "phantomjs" at' +
+                'the command line, this task should work). Unfortunately, ' +
+                'PhantomJS cannot be installed automatically via npm or grunt. ' +
+                'See the grunt FAQ for PhantomJS installation instructions: ' +
+                'https://github.com/cowboy/grunt/blob/master/docs/faq.md'
+        );
+    };
+
+    var runBusterServer = function () {
+        var deferred = when.defer(),
+            busterServerPath = path.resolve(__dirname, '../node_modules/.bin/buster-server');
+
+        var server = childProcess.spawn(busterServerPath, getArguments('server'), {
+            env: process.env,
+            setsid: true
+        });
+
+        server.stdout.once('data', function (data) {
+            deferred.resolve(server);
+        });
+
+        server.stderr.once('data', function (data) {
+            deferred.reject(server);
+        });
+
+        server.stdout.on('data', function (data) {
+            process.stdout.write(data);
+        });
+
+        server.stderr.on('data', function (data) {
+            process.stderr.write(data);
+        });
+
+        return deferred.promise;
+    };
+
+    var runBusterTest = function () {
+        var deferred = when.defer(),
+            busterTestPath = path.resolve(__dirname, '../node_modules/.bin/buster-test'),
+            output = [];
+
+        var run = childProcess.spawn(busterTestPath, getArguments('test'), {
+            env: process.env,
+            setsid: true
+        });
+
+        run.stdout.on('data', function (data) {
+            output.push(data);
+            process.stdout.write(data);
+        });
+
+        run.stderr.on('data', function (data) {
+            process.stderr.write(data);
+        });
+
+        run.on('exit', function (code) {
+            var text = '';
+            if (output[output.length - 2]) {
+                text = output[output.length - 2].toString().split(', ').join('\n') + output[output.length - 1];
+            }
+            text = text.replace(/\u001b\[.*m/g, '').trim();
+            if (code === 0) {
+                growl(text, {
+                    title: 'Tests Passed',
+                    image: __dirname + '/buster/ok.png'
+                });
+                deferred.resolve();
+            }
+            else {
+                growl(text, {
+                    title: 'Tests Failed',
+                    image: __dirname + '/buster/error.png'
+                });
+                deferred.reject();
+            }
+        });
+
+        return deferred.promise;
+    };
+
+    var runPhantomjs = function () {
+        var deferred = when.defer();
+        var server = childProcess.spawn(phantomjs.path, getArguments('phantomjs'), {
+            env: process.env,
+            setsid: true
+        });
+
+        server.stdout.on('data', function (data) {
+            grunt.verbose.writeln(data);
+        });
+
+        server.stderr.on('data', function (data) {
+            grunt.verbose.writeln(data);
+        });
+
+        server.stdout.once('data', function () {
+            deferred.resolve(server);
+        });
+
+        return deferred.promise;
+    };
+
+    grunt.registerTask('buster', 'Run Buster.JS tests.', function () {
+        var done = this.async();
+        var stop = function (success, server, phantomjs) {
+            if (server) {
+                server.kill();
+                grunt.verbose.writeln('buster-server stopped');
+            }
+            if (phantomjs) {
+                phantomjs.kill();
+                grunt.verbose.writeln('phantomjs stopped');
+            }
+            done(success);
+        };
+
+        if (shouldRunServer()) {
+            runBusterServer().then(
+                function (server) {
+                    runPhantomjs().then(
+                        function (phantomjs) {
+                            runBusterTest().then(
+                                function () {
+                                    stop(null, server, phantomjs);
+                                },
+                                function () {
+                                    stop(false, server, phantomjs);
+                                }
+                            );
+                        },
+                        function (phantomjs) {
+                            stop(false, server, phantomjs);
+                        }
+                    );
+                },
+                function (server) {
+                    stop(false, server);
+                }
+            );
+        }
+        else {
+            runBusterTest().then(
+                function () {
+                    done(null);
+                },
+                function () {
+                    done(false);
+                }
+            );
+        }
+    });
 };
